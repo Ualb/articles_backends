ea377f10ca36b6012650637c1c5d1bb0faefcf74
Ulises López <ulises050794@gmail.com>
2021-03-13 21:09:13 -0600

    modificaciones de nombres para el modelo de pagos

--- articles_backends/apps/payment/models.py
+++ articles_backends/apps/payment/models.py
@@ -14,11 +14,14 @@ class Payment(models.Model):
 class PaymentDetail(models.Model):
     id = models.AutoField(primary_key=True)
     # you do not have permission to delete payments or items by this way (on_delete)
-    code_article = models.ForeignKey(
+    id_article = models.ForeignKey(
         'article.Article', related_name='article', on_delete=models.DO_NOTHING)
     id_payment = models.ForeignKey(
         Payment, related_name='payment', on_delete=models.DO_NOTHING)
     quantity = models.IntegerField()
+    # ok, imagine the case, make a purchase and then you modify the cost of the list of items,
+    # then, the cost of the payment is modified, the solution is to save the subtotal
+    sub_total = models.DecimalField(max_digits=12, decimal_places=2)
 
     class Meta:
         verbose_name = "Payment Detail"
--- articles_backends/apps/payment/serializer.py
+++ articles_backends/apps/payment/serializer.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
+
 from .models import Payment, PaymentDetail
+from apps.article.serializer import ArticleSerializer
 
 
 class PaymentDetailSerializer(serializers.ModelSerializer):
+    articles = ArticleSerializer(many=False, read_only=True)
+
     class Meta:
         model = PaymentDetail
-        fields = '__all__'
+        fields = (
+            'id',
+            'id_article',
+            'id_payment',
+            'quantity',
+            'sub_total',
+            'articles'
+        )
 
 
 class PaymentSerializer(serializers.ModelSerializer):
